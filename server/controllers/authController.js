5cbec92b9e14e53ad9409bbc48fd1761a425f8ad
harshpathakzz <harshpathak2017@gmail.com>
2023-10-26 15:48:04 +0530

    updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,3 +21,23 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ server/utils/validateToken.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const validateToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (err) {
+      res.status(401).json({ message: "User is not authorized" });
+    }
+  } else {
+    res
+      .status(401)
+      .json({ message: "User is not authorized or token is missing" });
+  }
+};
+
+export default validateToken;
