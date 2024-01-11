872167e544ac106b7f04a280b110e9ea70541f7a
harshpathakzz <harshpathak2017@gmail.com>
2024-01-11 23:46:14 +0530

    update

--- client/src/components/component/CaseStudyCard.jsx
+++ client/src/components/component/CaseStudyCard.jsx
@@ -5,17 +5,15 @@ export default function CaseStudyCard({ title, coverImage }) {
     <Card className="shadow-lg max-w-md mx-auto">
       <img
         alt="Placeholder"
-        className="object-cover w-full h-60 rounded-t-lg"
-        height="200"
+        className="object-cover w-full h-50 rounded-t-lg"
         src={coverImage}
         style={{
-          aspectRatio: "400/200",
+          aspectRatio: "16/9",
           objectFit: "cover",
         }}
-        width="400"
       />
       <CardContent className="p-4">
-        <CardTitle className="text-lg font-semibold tracking-tight truncate ">
+        <CardTitle className="text-lg font-semibold tracking-tight truncate">
           {title}
         </CardTitle>
       </CardContent>
