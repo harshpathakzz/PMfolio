c1da5746919f5bfdfa1829c7d03377bdffa84e29
harshpathakzz <harshpathak2017@gmail.com>
2024-01-11 22:53:26 +0530

    updated route

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 const ProfilePage = () => {
   const { userId } = useParams();
   const [userData, setUserData] = useState(null);
+  const [userCaseStudies, setUserCaseStudies] = useState(null);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -20,19 +21,42 @@ const ProfilePage = () => {
       }
     };
 
+    const fetchUserCaseStudies = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/api/v1/case-studies/user/${userId}`
+        );
+        setUserCaseStudies(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
     fetchUserData();
+    fetchUserCaseStudies();
   }, [userId]);
 
   return (
     <div className="container mx-auto p-4">
       {userData ? (
-        <div className="flex items-center flex-col">
+        <div className="flex flex-col items-center">
           <Avatar className="mb-4 h-20 w-20">
             <AvatarImage src={userData.profilePicture} />
             <AvatarFallback>IMG</AvatarFallback>
           </Avatar>
           <h2 className="text-2xl font-bold">{userData.username}</h2>
           <p className="text-muted-foreground">{userData.bio}</p>
+          <div className="flex mt-2">
+            <p className="mr-4">
+              <span className="font-bold">{userData.followerCount}</span>{" "}
+              followers
+            </p>
+            <p>
+              <span className="font-bold">{userData.followingCount}</span>{" "}
+              following
+            </p>
+          </div>
         </div>
       ) : (
         <p className="text-center">Loading...</p>
