25f593fc0326d03cb504abff78aa9c200e72bfc5
harshpathakzz <harshpathak2017@gmail.com>
2024-01-13 16:44:18 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import connectDB from "./config/dbConnection.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import caseStudiesRoutes from "./routes/caseStudyRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
 
 config();
 
@@ -20,11 +21,12 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/case-studies", caseStudiesRoutes);
+app.use("/api/v1/comment", commentRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`)); // listen to port
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
 
 export default app;
