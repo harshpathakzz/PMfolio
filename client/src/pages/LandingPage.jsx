d79e3247629d8282c41d1174312969bda800d7f8
harshpathakzz <harshpathak2017@gmail.com>
2024-02-13 17:07:06 +0530

    updated

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -7,22 +7,27 @@ const LandingPage = () => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const navigate = useNavigate();
 
-  if (isLoggedIn) {
-    navigate("/feed");
-  }
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/feed");
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-background">
+    <div
+      className="min-h-screen flex flex-col justify-center items-center bg-background"
+      style={{ marginTop: "-20vh" }}
+    >
       <div className="max-w-lg mx-auto text-center">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl">
+        <h1 className="text-4xl font-extrabold tracking-tight text-primary lg:text-7xl">
           PMfolio
         </h1>
-        <p className="text-xl text-muted-foreground">
+        <p className="mt-4 text-xl text-muted-foreground">
           Connect with other product managers, share insights, and build your
           professional network.
         </p>
         <Button
-          className="w-1/2 bg-primary hover:bg-primary-foreground text-primary-foreground hover:text-primary rounded-lg py-3 px-6 shadow-md"
+          className="mt-8 w-1/2 bg-primary hover:bg-primary-foreground text-primary-foreground hover:text-primary rounded-lg py-3 px-6 shadow-md"
           onClick={() => {
             navigate("/feed");
           }}
